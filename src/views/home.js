e7c07667400b1ae8d91c339b8c98ee405f64a301
timwis <tim@timwis.com>
2017-01-24 06:23:33 -0500

    don't attempt to fetch repos if not logged in. closes #5

--- src/views/home.js
+++ src/views/home.js
@@ -3,9 +3,13 @@ const html = require('choo/html')
 const Repos = require('../components/repos')
 
 module.exports = (state, prev, send) => {
-  return html`
+  return state.user.login ? html`
     <div class="container" onload=${() => send('fetchRepos')}>
       ${state.repos.length ? Repos(state.repos) : ''}
+    </div>
+  ` : html`
+    <div class="container">
+      Login to view repos
     </div>
   `
 }
